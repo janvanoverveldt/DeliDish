f9ce4bec3a6454789a5162e3de9a815ddd52a39a
Janvo <jan.vanoverveldt@kdg.be>
2018-08-21 16:00:32 +0200

    FirstCommit

--- src/be/kdg/deliDish/domain/business/OrderCatalog.java
+++ src/be/kdg/deliDish/domain/business/OrderCatalog.java
@@ -1,4 +1,18 @@
 package be.kdg.deliDish.domain.business;
 
+import be.kdg.deliDish.domain.Order;
+import be.kdg.infra.MemoryRepository;
+
+import java.util.Collection;
+
 public class OrderCatalog {
+    private final MemoryRepository<Order> orders = new MemoryRepository<>();
+
+    public void addOrder(Order order) {
+        orders.put(order);
+    }
+
+    public Collection<Order> getOrders() {
+        return orders.entities();
+    }
 }

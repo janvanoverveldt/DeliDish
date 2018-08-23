42af07b3b0f4bb207fff79730d9536c3b5933073
Janvo <jan.vanoverveldt@kdg.be>
2018-08-23 10:11:21 +0200

    Eerste draft getAvailable deliveries beschikbaar. Testen en Testdata nog te beperkt om bugs uit te sluiten.

--- test/be/kdg/deliDish/application/DeliveryControllerTest.java
+++ test/be/kdg/deliDish/application/DeliveryControllerTest.java
@@ -1,9 +1,16 @@
 package be.kdg.deliDish.application;
 
-import be.kdg.deliDish.domain.business.AvailableDeliveriesService;
-import be.kdg.deliDish.domain.business.BelgianAvailableDeliveriesService;
-import be.kdg.deliDish.domain.order.Order;
-import org.junit.Before;
+import be.kdg.deliDish.business.AvailableDeliveriesService;
+import be.kdg.deliDish.business.BelgianAvailableDeliveriesService;
+import be.kdg.deliDish.business.domain.courier.Courier;
+import be.kdg.deliDish.business.domain.order.Order;
+import be.kdg.deliDish.business.domain.restaurant.Restaurant;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * In de test methoden van deze klasse mag je niks veranderen
@@ -11,32 +18,58 @@ import org.junit.Before;
  * @author Jan de Rijke.
  */
 public class DeliveryControllerTest {
-    private DeliveryController ctrl;
-    private AvailableDeliveriesService ads;
+    private static DeliveryController ctrl;
+    private static AvailableDeliveriesService ads;
 
-    @Before
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
         // TODO0 initialiseer het systeem
         TestData data = new TestData();
 
         ctrl = new DeliveryController();
-        ads = new BelgianAvailableDeliveriesService(ctrl.getCourierCatalog(), ctrl.getOrderCatalog());
+        ctrl.setAppUser(data.getCouriers().get(0));
+        ads = new BelgianAvailableDeliveriesService(ctrl.getCourierCatalog(), ctrl.getOrderCatalog(), ctrl.getRestoCatalog());
 
-        // TODO0 Voeg de testdata toe aan het systeem.
-        // De TestData zijn beschikbaar in collections via de getter methoden van TestData
-        // Na de initialisatie mag de TestData klasse niet meer gebruikt worden.
-        // Tijdens de test en in de project code moet al deze data opgezocht worden via het systeem
-        // 1. Contracten worden opgesteld.
+        //Load testdata into repositories
+        for (Courier courier : data.getCouriers()) {
+            ctrl.addCourier(courier);
+        }
+
+        ctrl.addCustomer(data.getCustomer());
+
+        for (Restaurant restaurant : data.getRestos()) {
+            ctrl.addResto(restaurant);
+        }
 
         for (Order order : data.getOrders()) {
             ctrl.addOrder(order);
         }
 
-        // 2. Producten worden opgesteld en aan contractperiodes gekoppeld.
-        // 3. Met behulp van de aangemaakte producten worden recepten opgesteld.
-
 
     }
 
 
+    @Test
+    void getAvailableDeliveries() {
+        Collection<Order> availableDeliveries = ctrl.getAvailableDeliveries(ads);
+        assertEquals(1, availableDeliveries.size(), "Slechts één order voldoet aan de voorwaarden");
+        assertEquals(ctrl.getOrderCatalog().getOrders().stream().findFirst().get(), availableDeliveries.stream().findFirst().get());
+
+    }
+
+    @Test
+    void selectDelivery() {
+    }
+
+    @Test
+    void registerDeliveryPickup() {
+    }
+
+    @Test
+    void registerSuccesfullDelivery() {
+    }
+
+    @Test
+    void getDeliveryPointsTotal() {
+    }
 }

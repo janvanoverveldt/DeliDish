ac25609704ac7394ba5f683f2505f3f960bea906
Janvo <jan.vanoverveldt@kdg.be>
2018-11-29 12:02:01 +0100

    Revisioned 2

--- src/be/kdg/distanceAPI/Point.java
+++ src/be/kdg/distanceAPI/Point.java
@@ -3,11 +3,11 @@ package be.kdg.distanceAPI;
 import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.shape.impl.PointImpl;
 
-public class Position extends PointImpl {
+public class Point extends PointImpl {
     /*
     Creates a point on earth.
      */
-    public Position(double lattitude, double longitude) {
+    public Point(double lattitude, double longitude) {
         super(lattitude, longitude, SpatialContext.GEO);
     }
 }

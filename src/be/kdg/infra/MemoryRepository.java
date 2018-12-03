2049acd1b57ebf4b545c9e8e9ee359fb84ad2803
jan <jan.derijke@kdg.be>
2018-12-03 18:07:00 +0100

    memorystorage to concurrenthashmap

--- src/be/kdg/infra/MemoryRepository.java
+++ src/be/kdg/infra/MemoryRepository.java
@@ -2,6 +2,7 @@ package be.kdg.infra;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,7 +17,7 @@ import java.util.stream.Stream;
  */
 public class MemoryRepository<K,V extends Serializable> implements Repository<K,V > {
 
-	private Map<K, byte[]> data = new HashMap<>();
+	private Map<K, byte[]> data = new ConcurrentHashMap<>();
 
 
 	@Override
